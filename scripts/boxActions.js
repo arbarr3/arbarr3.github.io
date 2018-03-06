70dd73b995ba0fe9e77aca87731718a410426b66
arbarr3 <arbarr3@gmail.com>
2018-03-05 21:09:34 -0500

    Most Action Done
    Fixed the display action, ready to start adding conent.

--- scripts/boxActions.js
+++ scripts/boxActions.js
@@ -1,31 +1,41 @@
 var currentlyExpanded = null;
 $(document).ready(function() {
-
-	var topLeft = $("#greenBox").position();
 	
 	$('.gameBox').click(function() {
 
+		//$(this).css({"position":"fixed","left":"0", "top":"10%"});
+		//$(this).animate({"width":"99%", "height":"100%"}, 1000);
 
 		var audioElem = document.createElement('audio');
 		switch(this.id) {
 			case 'greenBox':
 				audioElem.setAttribute('src', '/sounds/simonSound1.ogg');
-				$("#greenPage").css("display", "inline-block");
+				currentlyExpanded = $("#greenPage")
 				break;
 			case 'redBox':
 				audioElem.setAttribute('src', '/sounds/simonSound2.ogg');
+				currentlyExpanded = $("#redPage")
 				break;
 			case 'blueBox':
 				audioElem.setAttribute('src', '/sounds/simonSound3.ogg');
+				currentlyExpanded = $("#bluePage")
 				break;
 			case 'yellowBox':
 				audioElem.setAttribute('src', '/sounds/simonSound4.ogg');
+				currentlyExpanded = $("#yellowPage")
 				break;
 		}
 		audioElem.pause();
 		audioElem.currentTime = 0;
 		audioElem.play();
-		currentlyExpanded = this;
+
+		$(currentlyExpanded).css({"display":"block"});
+		$(currentlyExpanded).animate({"height":"82%"}, 1000);
 	});
 	
+	$('.collapse').click(function() {
+		$(currentlyExpanded).animate({"height":"0"}, 1000, "swing", function(){$(currentlyExpanded).css({"display":"none"});})
+	});
+
+
 });
